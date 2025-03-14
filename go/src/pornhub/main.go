4a353803e022edbc2a5293f965135e998362bf4f
MadPanda97 <19zainollinov97@gmail.com>
2025-03-14 14:50:47 +0800

    2 ver

--- go/src/pornhub/main.go
+++ go/src/pornhub/main.go
@@ -6,5 +6,25 @@ import (
 )
 
 func main() {
-	fmt.Println("Pisku dash ebat?")
+	var A = 5
+	var B = 4.5
+	S := A + int(B)
+	fmt.Println(S)
+
+	var V uint = 14
+	var pisyun byte = 25
+	GG := V * uint(pisyun)
+	fmt.Println(GG)
+
+	H := 13
+	J := 9
+	HJ := H & J
+	/* 13 =1101
+	9=1001
+	13&9 = 1001=9
+	*/
+	fmt.Println(HJ)
+	SPR := H | J
+	fmt.Println(SPR)
+
 }
