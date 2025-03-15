ea06d07a6a3446a91a33a64c0b535ff6ed40025e
MadPanda97 <19zainollinov97@gmail.com>
2025-03-15 21:14:23 +0800

    Version2

--- go/src/pornhub/main.go
+++ go/src/pornhub/main.go
@@ -1,76 +1,24 @@
 package main
 
 import (
-	"fmt"
 	_ "fmt"
 	_ "math"
 )
 
 func main() {
-	if A := 4; A == 1 {
-		fmt.Println("one")
-	} else if A == 2 {
-		fmt.Println("two")
-	} else if A == 3 {
-		fmt.Println("three")
-	} else if A == 4 {
-		fmt.Println("four")
-	} else if A == 5 {
-		fmt.Println("five")
-	} else if A == 6 {
-		fmt.Println("six")
-	} else if A == 7 {
-		fmt.Println("seven")
-	} else if A == 8 {
-		fmt.Println("eight")
-	} else if A == 9 {
-		fmt.Println("nine")
-	} else if A == 10 {
-		fmt.Println("ten")
-	} else if A == 0 {
-		fmt.Println("zero")
+	
+	var (
+		nFib1 = 1
+		nFib2 = 1 + 1
+	)
+
+	println(nFib1)
+	println(nFib2)
+
+	for n := 0; n < 30; n++ {
+		nFib1 = nFib2 + nFib1
+		println(nFib1)
+		nFib2 = nFib2 + nFib1
+		println(nFib2)
 	}
-
-	W1 := 5
-	L1 := 10
-	W2 := 6
-	L2 := 9
-
-	if W1 > W2 && L1 > L2 {
-		println("Вмещает")
-	} else {
-		println("Не вмещает")
-	}
-
-	pow := -5
-	if pow > 0 {
-		var N = 10
-		var result = N
-		for P := 0; P < pow-1; P++ {
-			result = result * N
-
-		}
-		{
-			println(result)
-		}
-	} else if pow == 0 {
-		var N = 1
-		var result = N
-		for P := 0; P < pow-1; P++ {
-			result = result * N
-		}
-		{
-			println(result)
-		}
-	} else if pow < 0 {
-		var N float64 = 10
-		var result float64 = N
-		for P := 0; P > pow-1; P-- {
-			result = result / N
-		}
-		{
-			println(result)
-		}
-	}
-
 }
